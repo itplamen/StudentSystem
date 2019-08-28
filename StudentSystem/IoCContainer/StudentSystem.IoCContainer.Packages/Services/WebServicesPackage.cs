846a7966ecc0c489bb7ffcf649c65b1133df11c7
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 01:53:23 +0300

    Created WebServicesPackage

---
+++ StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Services/WebServicesPackage.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentSystem.IoCContainer.Packages.Services
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.Services.Web;
+    using StudentSystem.Services.Web.Contracts;
+
+    public sealed class WebServicesPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IStudentsService, StudentsService>(Lifestyle.Transient);
+        }
+    }
+}
