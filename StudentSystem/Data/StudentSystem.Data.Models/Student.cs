4c1cd4c8d07cc4f0f61a7a5eceb103c09ecd5688
itplamen@gmail.com <itplamen@gmail.com>
2019-08-28 22:16:38 +0300

    Created Student data model

---
+++ StudentSystem/Data/StudentSystem.Data.Models/Student.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentSystem.Data.Models
+{
+    using System;
+
+    using StudentSystem.Data.Models.Base;
+    
+    public class Student : BaseModel
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
