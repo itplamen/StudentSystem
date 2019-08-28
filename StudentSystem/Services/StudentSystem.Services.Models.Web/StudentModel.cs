1cfe795cd8c23e7bdd3486dbe42a4e92686ff617
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 01:00:29 +0300

    Created Student web model

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/StudentModel.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentSystem.Services.Models.Web
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class StudentModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+
+        [DataMember]
+        public DateTime CreatedOn { get; set; }
+
+        [DataMember]
+        public DateTime? ModifiedOn { get; set; }
+
+        [DataMember]
+        public bool IsDeleted { get; set; }
+
+        [DataMember]
+        public DateTime? DeletedOn { get; set; }
+
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+
+        [DataMember]
+        public string Email { get; set; }
+
+        [DataMember]
+        public DateTime DateOfBirth { get; set; }
+    }
+}
