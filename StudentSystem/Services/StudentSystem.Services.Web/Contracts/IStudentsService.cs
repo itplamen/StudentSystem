eb4f70f1cb80855c524f37af387bd1be34ef9727
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 01:24:04 +0300

    Created simple students web service

---
+++ StudentSystem/Services/StudentSystem.Services.Web/Contracts/IStudentsService.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentSystem.Services.Web.Contracts
+{
+    using System.Collections.Generic;
+    using System.ServiceModel;
+
+    using StudentSystem.Services.Models.Web;
+
+    [ServiceContract]
+    public interface IStudentsService
+    {
+        [OperationContract]
+        IEnumerable<StudentModel> GetAll();
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Web/StudentsService.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentSystem.Services.Web
+{
+    using System.Collections.Generic;
+    using StudentSystem.Common.Contracts;
+
+    using StudentSystem.Data.Contracts.Queries;
+    using StudentSystem.Data.Models;
+    using StudentSystem.Services.Models.Web;
+    using StudentSystem.Services.Web.Contracts;
+
+    public class StudentsService : IStudentsService
+    {
+        private readonly IMapper<Student, StudentModel> studentsMapper;
+        private readonly IQueryHandler<IEnumerable<Student>> allStudentsHandler;
+
+        public StudentsService(IMapper<Student, StudentModel> studentsMapper, IQueryHandler<IEnumerable<Student>> allStudentsHandler)
+        {
+            this.studentsMapper = studentsMapper;
+            this.allStudentsHandler = allStudentsHandler;
+        }
+
+        public IEnumerable<StudentModel> GetAll()
+        {
+            IEnumerable<Student> students = allStudentsHandler.Handle();
+            IEnumerable<StudentModel> studentModels = studentsMapper.Map(students);
+
+            return studentModels;
+        }
+    }
+}
