326e10f4290f454a127cdd203105490f2ca4ea8d
itplamen@gmail.com <itplamen@gmail.com>
2019-08-28 22:16:18 +0300

    Created base data model and contracts

---
+++ StudentSystem/Data/StudentSystem.Data.Models/Base/BaseModel.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentSystem.Data.Models.Base
+{
+    using System;
+
+    public abstract class BaseModel : IAuditInfo, IDeletableEntity
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
---
+++ StudentSystem/Data/StudentSystem.Data.Models/Base/IAuditInfo.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Data.Models.Base
+{
+    using System;
+
+    public interface IAuditInfo
+    {
+        DateTime CreatedOn { get; set; }
+
+        DateTime? ModifiedOn { get; set; }
+    }
+}
