73c71d6d1c69b4f358ad9e76c72fa579804475e1
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 01:48:33 +0300

    Created QueryHandlersPackage

---
+++ StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Data/QueryHandlersPackage.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentSystem.IoCContainer.Packages.Data
+{
+    using System.Collections.Generic;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.Data.Contracts.Queries;
+    using StudentSystem.Data.Models;
+    using StudentSystem.Data.Queries.Students;
+
+    public sealed class QueryHandlersPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IQueryHandler<IEnumerable<Student>>, AllStudentsQueryHandler>(new WebRequestLifestyle());
+        }
+    }
+}
