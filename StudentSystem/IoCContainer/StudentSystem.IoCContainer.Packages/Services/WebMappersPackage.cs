c672babb3c966f1db058d9df14041ffc7ba40147
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 01:55:17 +0300

    Created WebMappersPackage

---
+++ StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Services/WebMappersPackage.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentSystem.IoCContainer.Packages.Services
+{
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Data.Models;
+    using StudentSystem.Services.Models.Web;
+    using StudentSystem.Services.Web.Mappers;
+
+    public sealed class WebMappersPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register(typeof(IMapper<Student, StudentModel>), typeof(StudentsMapper), Lifestyle.Singleton);
+        }
+    }
+}
