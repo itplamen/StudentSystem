a436e304f1d4d6c9119640c8537f73cbea5260ca
itplamen@gmail.com <itplamen@gmail.com>
2019-08-28 22:34:39 +0300

    Created common IMapper contract

---
+++ StudentSystem/Common/StudentSystem.Common/Contracts/IMapper.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Common.Contracts
+{
+    using System.Collections.Generic;
+
+    public interface IMapper<TFrom, To>
+        where TFrom : class
+        where To : class
+    {
+        To Map(TFrom from);
+    }
+}
