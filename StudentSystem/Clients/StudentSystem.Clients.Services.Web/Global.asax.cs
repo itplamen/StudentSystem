f1aaead583eeaf4b1276f152ed3e4ef4c814012f
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 20:29:54 +0300

    Added Global.asax and configured StudentsService.svc

---
+++ StudentSystem/Clients/StudentSystem.Clients.Services.Web/Global.asax.cs
@@ -0,0 +1,36 @@
+﻿namespace StudentSystem.Clients.Services.Web
+{
+    using System.Reflection;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Wcf;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.IoCContainer.Packages.Data;
+    using StudentSystem.IoCContainer.Packages.Services;
+
+    public class Global : System.Web.HttpApplication
+    {
+
+        protected void Application_Start()
+        {
+            Container container = new Container();
+            container.RegisterWcfServices(Assembly.GetExecutingAssembly());
+
+            IPackage[] packages = new IPackage[]
+            {
+                new DataMappersPackage(),
+                new QueryHandlersPackage(),
+                new WebMappersPackage(),
+                new WebServicesPackage()
+            };
+
+            foreach (var package in packages)
+            {
+                package.RegisterServices(container);
+            }
+
+            SimpleInjectorServiceHostFactory.SetContainer(container);
+        }
+    }
+}
