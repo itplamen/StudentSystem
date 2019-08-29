7b34369930c86f80da1be975d3457d452c995e22
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 23:18:46 +0300

    Installed SimpleInjector v.4.6.2 NuGet packages to StudentSystem.Clients.Web project and configured Global.asax

--- StudentSystem/Clients/StudentSystem.Clients.Web/Global.asax.cs
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Global.asax.cs
@@ -1,17 +1,38 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
-using System.Web.Optimization;
-using System.Web.Routing;
-
-namespace StudentSystem.Clients.Web
+﻿namespace StudentSystem.Clients.Web
 {
+    using System.Reflection;
+    using System.Web.Mvc;
+    using System.Web.Optimization;
+    using System.Web.Routing;
+
+    using SimpleInjector;
+    using SimpleInjector.Integration.Web.Mvc;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.IoCContainer.Packages.Common;
+
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
         {
+            Container container = new Container();
+
+            IPackage[] packages = new IPackage[]
+            {
+                new ApiPackage(),
+                new CommonPackage()
+            };
+
+            foreach (var package in packages)
+            {
+                package.RegisterServices(container);
+            }
+
+            container.RegisterMvcControllers(Assembly.GetExecutingAssembly());
+            container.Verify();
+
+            DependencyResolver.SetResolver(new SimpleInjectorDependencyResolver(container));
+
             AreaRegistration.RegisterAllAreas();
             FilterConfig.RegisterGlobalFilters(GlobalFilters.Filters);
             RouteConfig.RegisterRoutes(RouteTable.Routes);
