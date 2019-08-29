a835863b874b9c95342453aa1b18c40903d218d3
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 23:11:32 +0300

    Created common ApiPackage

---
+++ StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Common/ApiPackage.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentSystem.IoCContainer.Packages.Common
+{
+    using System.Net.Http;
+
+    using SimpleInjector;
+    using SimpleInjector.Packaging;
+
+    using StudentSystem.Services.Api;
+    using StudentSystem.Services.Api.Contracts;
+
+    public sealed class ApiPackage : IPackage
+    {
+        public void RegisterServices(Container container)
+        {
+            container.Register<IStudentSystemApi, StudentSystemApi>(Lifestyle.Singleton);
+            container.RegisterDecorator<IStudentSystemApi, StudentSystemApiLoggingDecorator>(Lifestyle.Singleton);
+
+            container.Register(() => new HttpClient(), Lifestyle.Singleton);
+        }
+    }
+}
