c361e84e76adbfd2936f04bcfea53e46bcd8cba3
itplamen@gmail.com <itplamen@gmail.com>
2019-08-30 00:36:50 +0300

    Created SqlQueryExecutor

---
+++ StudentSystem/Data/StudentSystem.Data/Contracts/ISqlQueryExecutor.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentSystem.Data.Contracts
+{
+    using System;
+    using System.Data.SqlClient;
+
+    public interface ISqlQueryExecutor
+    {
+        T Execute<T>(string sqlQuery, Func<SqlCommand, T> funcQuery);
+    }
+}
---
+++ StudentSystem/Data/StudentSystem.Data/SqlQueryExecutor.cs
@@ -0,0 +1,31 @@
+﻿namespace StudentSystem.Data
+{
+    using System;
+    using System.Data.SqlClient;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Data.Contracts;
+
+    public class SqlQueryExecutor : ISqlQueryExecutor
+    {
+        private readonly IConfigurationManager configurationManager;
+
+        public SqlQueryExecutor(IConfigurationManager configurationManager)
+        {
+            this.configurationManager = configurationManager;
+        }
+
+        public T Execute<T>(string sqlQuery, Func<SqlCommand, T> funcQuery)
+        {
+            using (SqlConnection connection = new SqlConnection(configurationManager.ConnectionString))
+            {
+                connection.Open();
+
+                using (SqlCommand sqlCommand = new SqlCommand(sqlQuery, connection))
+                {
+                    return funcQuery(sqlCommand);
+                }
+            }
+        }
+    }
+}
