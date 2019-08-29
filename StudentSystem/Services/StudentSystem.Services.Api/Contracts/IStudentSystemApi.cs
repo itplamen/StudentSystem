7baa6e9ef7f8992f20c383f9429229f1fa0a4ac7
itplamen@gmail.com <itplamen@gmail.com>
2019-08-29 22:40:41 +0300

    Created StudentSystemApi

---
+++ StudentSystem/Services/StudentSystem.Services.Api/Contracts/IStudentSystemApi.cs
@@ -0,0 +1,10 @@
+﻿namespace StudentSystem.Services.Api.Contracts
+{
+    using System.Threading.Tasks;
+
+    public interface IStudentSystemApi
+    {
+        Task<T> GetAsync<T>(string url)
+            where T : class;
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Api/StudentSystemApi.cs
@@ -0,0 +1,34 @@
+﻿namespace StudentSystem.Services.Api
+{
+    using System.Net.Http;
+    using System.Net.Http.Headers;
+    using System.Threading.Tasks;
+
+    using Newtonsoft.Json;
+
+    using StudentSystem.Services.Api.Contracts;
+
+    public class StudentSystemApi : IStudentSystemApi
+    {
+        private const string DEFAULT_MEDIA_TYPE_HEADER = "application/json";
+
+        private readonly HttpClient httpClient;
+
+        public StudentSystemApi(HttpClient httpClient)
+        {
+            this.httpClient = httpClient;
+            this.httpClient.DefaultRequestHeaders.Add("Connection", "close");
+            this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue(DEFAULT_MEDIA_TYPE_HEADER));
+        }
+
+        public async Task<T> GetAsync<T>(string url)
+            where T : class
+        {
+            HttpResponseMessage responseMessage = await this.httpClient.GetAsync(url);
+            string jsonString = await responseMessage.Content.ReadAsStringAsync();
+            T response = JsonConvert.DeserializeObject<T>(jsonString);
+
+            return response;
+        }
+    }
+}
