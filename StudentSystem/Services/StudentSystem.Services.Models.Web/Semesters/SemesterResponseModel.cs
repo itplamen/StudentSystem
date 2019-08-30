7c5aa905230c31b4af3512cc9066bb7716af0060
itplamen@gmail.com <itplamen@gmail.com>
2019-08-30 23:49:37 +0300

    Created SemesterResponseModel

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Semesters/SemesterResponseModel.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentSystem.Services.Models.Web.Semesters
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    using StudentSystem.Services.Models.Web.Base;
+
+    [DataContract]
+    public class SemesterResponseModel : BaseResponseModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+    }
+}
