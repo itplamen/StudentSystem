6d2e5a77598d0d184f8d60207fc862a71558a15f
itplamen@gmail.com <itplamen@gmail.com>
2019-08-30 23:53:25 +0300

    Created ScoreResponseModel

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Scores/ScoreResponseModel.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentSystem.Services.Models.Web.Scores
+{
+    using System.Runtime.Serialization;
+
+    using StudentSystem.Services.Models.Web.Base;
+
+    [DataContract]
+    public class ScoreResponseModel : BaseResponseModel
+    {
+        [DataMember]
+        public string Discipline { get; set; }
+
+        [DataMember]
+        public string Professor { get; set; }
+
+        [DataMember]
+        public float Score { get; set; }
+    }
+}
