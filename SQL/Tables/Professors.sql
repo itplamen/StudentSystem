c2fb30883503772748048d63bd1f74b416957056
itplamen@gmail.com <itplamen@gmail.com>
2019-08-30 21:46:47 +0300

    Created Professors table

---
+++ SQL/Tables/Professors.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Professors(
+    Id INT IDENTITY NOT NULL,
+    FirstName NVARCHAR(50) NOT NULL,
+	LastName NVARCHAR(50) NOT NULL,
+	ModifiedOn DATETIME NULL,
+	IsDeleted BIT NOT NULL DEFAULT(0),
+	DeletedOn DATETIME NULL,
+	CONSTRAINT PK_Professors PRIMARY KEY CLUSTERED (Id ASC)
+);
+GO
+
+SET IDENTITY_INSERT Professors ON
+
+INSERT INTO Professors(Id, FirstName, LastName)
+VALUES (1, 'Jossef', 'Goldberg')
+
+INSERT INTO Professors(Id, FirstName, LastName)
+VALUES (2, 'Sidney', 'Higa')
+
+INSERT INTO Professors(Id, FirstName, LastName)
+VALUES (3, 'Taylor', 'Maxwell')
+
+SET IDENTITY_INSERT Professors OFF
+GO
