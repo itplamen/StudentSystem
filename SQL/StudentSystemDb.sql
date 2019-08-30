5d7e45c2815078adf80069c8b0da1370081e70cc
itplamen@gmail.com <itplamen@gmail.com>
2019-08-30 21:47:14 +0300

    Added small improvements to some tables

--- SQL/StudentSystemDb.sql
+++ SQL/StudentSystemDb.sql
@@ -4,8 +4,5 @@ GO
 CREATE DATABASE StudentSystemDb
 GO
 
-PRINT 'Created StudentSystemDb database'
-GO
-
 USE StudentSystemDb
 GO
--- SQL/Tables/Disciplines.sql
+++ SQL/Tables/Disciplines.sql
@@ -1,46 +1,50 @@
 CREATE TABLE Disciplines(
     Id INT IDENTITY NOT NULL,
     Name NVARCHAR(100) NOT NULL,
+	SemesterId INT NOT NULL,
+	ProfessorId INT NOT NULL,
 	CreatedOn DATETIME NOT NULL DEFAULT(GETDATE()),
 	ModifiedOn DATETIME NULL,
 	IsDeleted BIT NOT NULL DEFAULT(0),
 	DeletedOn DATETIME NULL,
 	CONSTRAINT PK_Disciplines PRIMARY KEY CLUSTERED (Id ASC),
-	CONSTRAINT AK_Disciplines_Name UNIQUE NONCLUSTERED (Name)
+	CONSTRAINT AK_Disciplines_Name UNIQUE NONCLUSTERED (Name),
+	CONSTRAINT FK_Disciplines_Semesters FOREIGN KEY (SemesterId) REFERENCES Semesters(Id),
+	CONSTRAINT FK_Disciplines_Professors FOREIGN KEY (ProfessorId) REFERENCES Professors(Id)
 );
 GO
 
 SET IDENTITY_INSERT Disciplines ON
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (1, 'Programming Fundamentals')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (1, 'Programming Fundamentals', 1, 1)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (2, 'C# OOP')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (2, 'C# OOP', 1, 1)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (3, 'Linux System Administration')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (3, 'Linux System Administration', 2, 1)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (4, 'Ethical Hacking')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (4, 'Ethical Hacking', 2, 2)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (5, 'HTML & CSS')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (5, 'HTML & CSS', 3, 2)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (6, 'Quality Assurance')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (6, 'Quality Assurance', 3, 2)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (7, 'Windows System Administration')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (7, 'Windows System Administration', 4, 3)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (8, 'Microcontrollers')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (8, 'Microcontrollers', 4, 3)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (9, 'C++ Advanced')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (9, 'C++ Advanced', 5, 3)
 
-INSERT INTO Disciplines(Id, Name)
-VALUES (10, 'WPF Essentials')
+INSERT INTO Disciplines(Id, Name, SemesterId, ProfessorId)
+VALUES (10, 'WPF Essentials', 6, 3)
 
 SET IDENTITY_INSERT Disciplines OFF
 GO
