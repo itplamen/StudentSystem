a6007d68dd83d1051f609eef3622e332fcc8245a
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 12:27:11 +0300

    Created SemestersBuilder

---
+++ StudentSystem/Data/StudentSystem.Data/Builders/StudentDetails/SemestersBuilder.cs
@@ -0,0 +1,35 @@
+﻿namespace StudentSystem.Data.Builders.StudentDetails
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Data.Contracts.Builders;
+    using StudentSystem.Data.Models;
+
+    public class SemestersBuilder : IBuilder<SqlDataReader, Semester>
+    {        
+        private readonly IMapper<SqlDataReader, Semester> semestersMapper;
+        private readonly IBuilder<SqlDataReader, Discipline> disciplinesBuilder;
+
+        public SemestersBuilder(IMapper<SqlDataReader, Semester> semestersMapper, IBuilder<SqlDataReader, Discipline> disciplinesBuilder)
+        {
+            this.semestersMapper = semestersMapper;
+            this.disciplinesBuilder = disciplinesBuilder;
+        }
+
+        public IEnumerable<Semester> Build(IEnumerable<SqlDataReader> from)
+        {
+            IEnumerable<Semester> semesters = semestersMapper.Map(from);
+            IEnumerable<Discipline> disciplines = disciplinesBuilder.Build(from);
+
+            foreach (var semester in semesters)
+            {
+                semester.Disciplines = disciplines.Where(x => x.SemesterId == semester.Id);
+            }
+
+            return semesters;
+        }
+    }
+}
