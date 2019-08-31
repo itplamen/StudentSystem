9ee0d473b67d40bd65dfc203f5f3988432e9a164
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 12:26:58 +0300

    Created DisciplinesBuilder

---
+++ StudentSystem/Data/StudentSystem.Data/Builders/StudentDetails/DisciplinesBuilder.cs
@@ -0,0 +1,45 @@
+﻿namespace StudentSystem.Data.Builders.StudentDetails
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Data.Contracts.Builders;
+    using StudentSystem.Data.Models;
+
+    public class DisciplinesBuilder : IBuilder<SqlDataReader, Discipline>
+    {
+        private readonly IBuilder<SqlDataReader, Score> scoresBuilder;
+        private readonly IBuilder<SqlDataReader, Professor> professorsBuilder;
+        private readonly IMapper<SqlDataReader, Discipline> disciplinesMapper;
+
+        public DisciplinesBuilder(
+            IBuilder<SqlDataReader, Score> scoresBuilder,
+            IBuilder<SqlDataReader, Professor> professorsBuilder,
+            IMapper<SqlDataReader, Discipline> disciplinesMapper)
+        {
+            this.scoresBuilder = scoresBuilder;
+            this.professorsBuilder = professorsBuilder;
+            this.disciplinesMapper = disciplinesMapper;
+        }
+
+        public IEnumerable<Discipline> Build(IEnumerable<SqlDataReader> from)
+        {
+            if (from.First().NextResult())
+            {
+                IEnumerable<Discipline> disciplines = disciplinesMapper.Map(from);
+                IEnumerable<Professor> professors = professorsBuilder.Build(from);
+                IEnumerable<Score> scores = scoresBuilder.Build(from);
+
+                foreach (var discipline in disciplines)
+                {
+                    discipline.Professor = professors.FirstOrDefault(x => x.Id == discipline.ProfessorId);
+                    discipline.Scores = scores.Where(x => x.DisciplineId == discipline.Id);
+                }
+            }
+
+            return Enumerable.Empty<Discipline>();
+        }
+    }
+}
