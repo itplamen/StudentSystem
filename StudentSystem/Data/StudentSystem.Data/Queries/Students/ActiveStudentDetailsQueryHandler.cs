276ce9a9de9663851c81859fefe6ae38ad28c9b6
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 12:33:40 +0300

    Updated student details query handler

---
+++ StudentSystem/Data/StudentSystem.Data/Queries/Students/ActiveStudentDetailsQueryHandler.cs
@@ -0,0 +1,44 @@
+﻿namespace StudentSystem.Data.Queries.Students
+{
+    using System.Collections.Generic;
+    using System.Data;
+    using System.Data.SqlClient;
+
+    using StudentSystem.Data.Contracts;
+    using StudentSystem.Data.Contracts.Builders;
+    using StudentSystem.Data.Contracts.Queries;
+    using StudentSystem.Data.Models;
+
+    public class ActiveStudentDetailsQueryHandler : IQueryHandler<IEnumerable<Semester>>
+    {
+        private readonly ISqlQueryExecutor sqlQueryExecutor;
+        private readonly IBuilder<SqlDataReader, Semester> studentDetailsBuilder;
+
+        public ActiveStudentDetailsQueryHandler(ISqlQueryExecutor sqlQueryExecutor, IBuilder<SqlDataReader, Semester> studentDetailsBuilder)
+        {
+            this.sqlQueryExecutor = sqlQueryExecutor;
+            this.studentDetailsBuilder = studentDetailsBuilder;
+        }
+
+        public IEnumerable<Semester> Handle()
+        {
+            string query = "sp_GetActiveStudentDetails";
+            IEnumerable<Semester> students = sqlQueryExecutor.Execute(query, GetDetails);
+
+            return students;
+        }
+
+        private IEnumerable<Semester> GetDetails(SqlCommand sqlCommand)
+        {
+            sqlCommand.CommandType = CommandType.StoredProcedure;
+
+            using (SqlDataReader reader = sqlCommand.ExecuteReader())
+            {
+                IEnumerable<SqlDataReader> readers = new List<SqlDataReader>() { reader };
+                IEnumerable<Semester> studentDetails = studentDetailsBuilder.Build(readers);
+
+                return studentDetails;
+            }
+        }
+    }
+}
--- StudentSystem/Services/StudentSystem.Services.Web/StudentsService.cs
+++ StudentSystem/Services/StudentSystem.Services.Web/StudentsService.cs
@@ -1,7 +1,7 @@
 ﻿namespace StudentSystem.Services.Web
 {
     using System.Collections.Generic;
-
+    using System.Linq;
     using StudentSystem.Common.Contracts;
     using StudentSystem.Data.Contracts.Queries;
     using StudentSystem.Data.Models;
@@ -11,20 +11,22 @@
     public class StudentsService : IStudentsService
     {
         private readonly IMapper<Student, StudentResponseModel> studentsMapper;
-        private readonly IQueryHandler<IEnumerable<Student>> allStudentsHandler;
+        private readonly IQueryHandler<IEnumerable<Semester>> activeStudentDetailsHandler;
 
-        public StudentsService(IMapper<Student, StudentResponseModel> studentsMapper, IQueryHandler<IEnumerable<Student>> allStudentsHandler)
+        public StudentsService(
+            IMapper<Student, StudentResponseModel> studentsMapper, 
+            IQueryHandler<IEnumerable<Semester>> activeStudentDetailsHandler)
         {
             this.studentsMapper = studentsMapper;
-            this.allStudentsHandler = allStudentsHandler;
+            this.activeStudentDetailsHandler = activeStudentDetailsHandler;
         }
 
         public IEnumerable<StudentResponseModel> GetAll()
         {
-            IEnumerable<Student> students = allStudentsHandler.Handle();
-            IEnumerable<StudentResponseModel> studentModels = studentsMapper.Map(students);
+            IEnumerable<Semester> studentDetails = activeStudentDetailsHandler.Handle();
+            IEnumerable<Score> scores = studentDetails.SelectMany(x => x.Disciplines.SelectMany(y => y.Scores));
 
-            return studentModels;
+            return null;
         }
     }
 }
