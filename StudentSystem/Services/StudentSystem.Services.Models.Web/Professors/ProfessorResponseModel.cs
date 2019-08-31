b8d77a4c91bf6d6ea4ddcc17017624da06ef37af
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 15:25:34 +0300

    Created ProfessorResponseModel

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Professors/ProfessorResponseModel.cs
@@ -0,0 +1,16 @@
+﻿namespace StudentSystem.Services.Models.Web.Professors
+{
+    using System.Runtime.Serialization;
+
+    using StudentSystem.Services.Models.Web.Base;
+
+    [DataContract]
+    public class ProfessorResponseModel : BaseResponseModel
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
