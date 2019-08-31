092959fc23caef8580831aacf2972497b414283f
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 12:12:45 +0300

    Created IBuilder data contract

---
+++ StudentSystem/Data/StudentSystem.Data/Contracts/Builders/IBuilder.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Data.Contracts.Builders
+{
+    using System.Collections.Generic;
+
+    public interface IBuilder<TFrom, To>
+        where TFrom : class
+        where To : class
+    {
+        IEnumerable<To> Build(IEnumerable<TFrom> from);
+    }
+}
