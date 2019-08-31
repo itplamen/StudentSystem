76932af82f8571cf30a6aa7ab054b6552faf2e37
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 15:29:35 +0300

    Created DisciplineResponseModel

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Disciplines/DisciplineResponseModel.cs
@@ -0,0 +1,28 @@
+﻿namespace StudentSystem.Services.Models.Web.Disciplines
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    using StudentSystem.Services.Models.Web.Base;
+    using StudentSystem.Services.Models.Web.Professors;
+    using StudentSystem.Services.Models.Web.Scores;
+
+    [DataContract]
+    public class DisciplineResponseModel : BaseResponseModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int SemesterId { get; set; }
+
+        [DataMember]
+        public int ProfessorId { get; set; }
+
+        [DataMember]
+        public ProfessorResponseModel Professor { get; set; }
+
+        [DataMember]
+        public IEnumerable<ScoreResponseModel> Scores { get; set; }
+    }
+}
