fd27ee569ca4cfc2311c43490065541781d4bc25
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 18:44:20 +0300

    Removed Http requests tot the WCF services

--- StudentSystem/Services/StudentSystem.Services.Web/Contracts/IStudentsService.cs
+++ StudentSystem/Services/StudentSystem.Services.Web/Contracts/IStudentsService.cs
@@ -2,14 +2,12 @@
 {
     using System.Collections.Generic;
     using System.ServiceModel;
-    using System.ServiceModel.Web;
 
     using StudentSystem.Services.Models.Web.Semesters;
 
     [ServiceContract]
     public interface IStudentsService
     {
-        [WebGet(ResponseFormat = WebMessageFormat.Json)]
         [OperationContract]
         IEnumerable<SemesterResponseModel> Get();
     }
