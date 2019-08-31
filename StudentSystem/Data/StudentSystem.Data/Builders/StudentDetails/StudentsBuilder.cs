f675ff7146ae093255bad2bf3e1c07a1cce11b59
itplamen@gmail.com <itplamen@gmail.com>
2019-08-31 12:25:34 +0300

    Created StudentsBuilder

---
+++ StudentSystem/Data/StudentSystem.Data/Builders/StudentDetails/StudentsBuilder.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentSystem.Data.Builders.StudentDetails
+{
+    using System.Collections.Generic;
+    using System.Data.SqlClient;
+    using System.Linq;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Data.Contracts.Builders;
+    using StudentSystem.Data.Models;
+
+    public class StudentsBuilder : IBuilder<SqlDataReader, Student>
+    {
+        private readonly IMapper<SqlDataReader, Student> studentsMapper;
+
+        public StudentsBuilder(IMapper<SqlDataReader, Student> studentsMapper)
+        {
+            this.studentsMapper = studentsMapper;
+        }
+
+        public IEnumerable<Student> Build(IEnumerable<SqlDataReader> from)
+        {
+            if (from.First().NextResult())
+            {
+                IEnumerable<Student> students = studentsMapper.Map(from);
+
+                return students;
+            }
+
+            return Enumerable.Empty<Student>();
+        }
+    }
+}
