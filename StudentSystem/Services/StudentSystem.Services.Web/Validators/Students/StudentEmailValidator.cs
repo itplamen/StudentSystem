a00ec44deeb227758aa495a8c98f0cffabfd2b28
itplamen@gmail.com <itplamen@gmail.com>
2019-09-01 14:42:59 +0300

    Created StudentEmailValidator

---
+++ StudentSystem/Services/StudentSystem.Services.Web/Validators/Students/StudentEmailValidator.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentSystem.Services.Web.Validators.Students
+{
+    using System.Text.RegularExpressions;
+
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Common.Validators;
+    using StudentSystem.Services.Models.Web.Students;
+
+    public class StudentEmailValidator : IValidator<StudentRequestModel>
+    {
+        private const string EMAIL_REGEX = @"(@)(.+)$";
+
+        public ValidationResult Validate(StudentRequestModel model)
+        {
+            if (string.IsNullOrEmpty(model.Email) || !Regex.Match(model.Email, EMAIL_REGEX).Success)
+            {
+                return new ValidationResult("Emails is invalid.");
+            }
+
+            return new ValidationResult();
+        }
+    }
+}
