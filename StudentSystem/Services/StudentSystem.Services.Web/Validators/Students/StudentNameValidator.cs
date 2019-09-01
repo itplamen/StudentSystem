863ebf9671a5d3e2283db01e062958e1732efe5a
itplamen@gmail.com <itplamen@gmail.com>
2019-09-01 14:44:27 +0300

    Created StudentNameValidator

---
+++ StudentSystem/Services/StudentSystem.Services.Web/Validators/Students/StudentNameValidator.cs
@@ -0,0 +1,19 @@
+﻿namespace StudentSystem.Services.Web.Validators.Students
+{
+    using StudentSystem.Common.Contracts;
+    using StudentSystem.Common.Validators;
+    using StudentSystem.Services.Models.Web.Students;
+
+    public class StudentNameValidator : IValidator<StudentRequestModel>
+    {
+        public ValidationResult Validate(StudentRequestModel model)
+        {
+            if (string.IsNullOrEmpty(model.FirstName) || string.IsNullOrEmpty(model.LastName))
+            {
+                return new ValidationResult("Student name is invalid!");
+            }
+
+            return new ValidationResult();
+        }
+    }
+}
