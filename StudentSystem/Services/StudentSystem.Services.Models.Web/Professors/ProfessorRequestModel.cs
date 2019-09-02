73926831b4223399effe0a159163cbdc6517c0d7
itplamen@gmail.com <itplamen@gmail.com>
2019-09-02 21:13:21 +0300

    Add request models for creating and updating professors

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Professors/ProfessorRequestModel.cs
@@ -0,0 +1,14 @@
+﻿namespace StudentSystem.Services.Models.Web.Professors
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ProfessorRequestModel
+    {
+        [DataMember]
+        public string FirstName { get; set; }
+
+        [DataMember]
+        public string LastName { get; set; }
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Professors/UpdateProfessorRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Services.Models.Web.Professors
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UpdateProfessorRequestModel : ProfessorRequestModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+    }
+}
