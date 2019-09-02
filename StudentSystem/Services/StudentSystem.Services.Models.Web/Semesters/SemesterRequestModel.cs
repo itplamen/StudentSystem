532e2c3bcf9c914fe461c239fc1900ef909aa185
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 00:38:58 +0300

    Created semester request models

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Semesters/SemesterRequestModel.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentSystem.Services.Models.Web.Semesters
+{
+    using System;
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class SemesterRequestModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public DateTime StartDate { get; set; }
+
+        [DataMember]
+        public DateTime EndDate { get; set; }
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Semesters/UpdateSemesterRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Services.Models.Web.Semesters
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UpdateSemesterRequestModel : SemesterRequestModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+    }
+}
