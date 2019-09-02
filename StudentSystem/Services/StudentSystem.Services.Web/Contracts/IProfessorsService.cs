8ca08b19938992fac50ef537977e14a2da99de8b
itplamen@gmail.com <itplamen@gmail.com>
2019-09-02 21:18:45 +0300

    Add ProfessorsService with endpoint for creating professors

---
+++ StudentSystem/Services/StudentSystem.Services.Web/Contracts/IProfessorsService.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentSystem.Services.Web.Contracts
+{
+    using System.ServiceModel;
+
+    using StudentSystem.Services.Models.Web.Professors;
+
+    [ServiceContract]
+    public interface IProfessorsService
+    {
+        [OperationContract]
+        bool Create(ProfessorRequestModel request);
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Web/ProfessorsService.cs
@@ -0,0 +1,25 @@
+﻿namespace StudentSystem.Services.Web
+{
+    using StudentSystem.Data.Commands.Professors;
+    using StudentSystem.Data.Contracts.Commands;
+    using StudentSystem.Services.Models.Web.Professors;
+    using StudentSystem.Services.Web.Contracts;
+
+    public class ProfessorsService : IProfessorsService
+    {
+        private readonly ICommandHandler<ProfessorCommand, int> createProfessorHandler;
+
+        public ProfessorsService(ICommandHandler<ProfessorCommand, int> createProfessorHandler)
+        {
+            this.createProfessorHandler = createProfessorHandler;
+        }
+
+        public bool Create(ProfessorRequestModel request)
+        {
+            ProfessorCommand command = new ProfessorCommand(request.FirstName, request.LastName);
+            int id = createProfessorHandler.Handle(command);
+
+            return id > 0;
+        }
+    }
+}
