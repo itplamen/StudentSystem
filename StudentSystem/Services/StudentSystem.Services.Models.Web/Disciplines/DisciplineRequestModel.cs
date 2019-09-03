01a07d2d0c3db70c82546b64925f679d8999285b
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 22:15:45 +0300

    Created disciplines request models

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Disciplines/DisciplineRequestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentSystem.Services.Models.Web.Disciplines
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class DisciplineRequestModel
+    {
+        [DataMember]
+        public string Name { get; set; }
+
+        [DataMember]
+        public int SemesterId { get; set; }
+
+        [DataMember]
+        public int ProfessorId { get; set; }
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Disciplines/UpdateDisciplineRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Services.Models.Web.Disciplines
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UpdateDisciplineRequestModel : DisciplineRequestModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+    }
+}
