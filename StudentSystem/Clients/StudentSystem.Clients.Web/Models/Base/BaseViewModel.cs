df22169da7858dfbe5454a84645b41bd0aeb2e72
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 23:55:51 +0300

    Created BaseViewModel

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Base/BaseViewModel.cs
@@ -0,0 +1,20 @@
+﻿namespace StudentSystem.Clients.Web.Models.Base
+{
+    using System;
+
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.DisciplinesServiceSoap;
+
+    public abstract class BaseViewModel : IMapFrom<BaseResponseModel>
+    {
+        public int Id { get; set; }
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? ModifiedOn { get; set; }
+
+        public bool IsDeleted { get; set; }
+
+        public DateTime? DeletedOn { get; set; }
+    }
+}
