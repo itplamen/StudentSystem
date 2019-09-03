2454a55046c8faaa8682a97be30f015c8870a0bc
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 22:59:52 +0300

    Created score request models

---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Scores/ScoreRequestModel.cs
@@ -0,0 +1,17 @@
+﻿namespace StudentSystem.Services.Models.Web.Scores
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class ScoreRequestModel
+    {
+        [DataMember]
+        public float Mark { get; set; }
+
+        [DataMember]
+        public int StudentId { get; set; }
+
+        [DataMember]
+        public int DisciplineId { get; set; }
+    }
+}
---
+++ StudentSystem/Services/StudentSystem.Services.Models.Web/Scores/UpdateScoreRequestModel.cs
@@ -0,0 +1,11 @@
+﻿namespace StudentSystem.Services.Models.Web.Scores
+{
+    using System.Runtime.Serialization;
+
+    [DataContract]
+    public class UpdateScoreRequestModel : ScoreRequestModel
+    {
+        [DataMember]
+        public int Id { get; set; }
+    }
+}
