3551d0a92a183e6cb92d3bcc1c16464e3996cca6
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 23:59:56 +0300

    Created ProfessorViewModel

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Professors/ProfessorViewModel.cs
@@ -0,0 +1,13 @@
+﻿namespace StudentSystem.Clients.Web.Models.Professors
+{
+    using StudentSystem.Clients.Web.Models.Base;
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.ProfessorsServiceSoap;
+
+    public class ProfessorViewModel : BaseViewModel, IMapFrom<ProfessorResponseModel>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
