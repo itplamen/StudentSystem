06f022098d0f4b050bcf694262f02e87679d7780
itplamen@gmail.com <itplamen@gmail.com>
2019-09-03 23:40:32 +0300

    Registered missing service clients

--- StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Common/ApiPackage.cs
+++ StudentSystem/IoCContainer/StudentSystem.IoCContainer.Packages/Common/ApiPackage.cs
@@ -5,7 +5,10 @@
 
     using StudentSystem.Services.Api;
     using StudentSystem.Services.Api.Contracts;
+    using StudentSystem.Services.Api.DisciplinesServiceSoap;
     using StudentSystem.Services.Api.ProfessorsServiceSoap;
+    using StudentSystem.Services.Api.ScoresServiceSoap;
+    using StudentSystem.Services.Api.SemestersServiceSoap;
     using StudentSystem.Services.Api.StudentsServiceSoap;
 
     public sealed class ApiPackage : IPackage
@@ -15,8 +18,11 @@
             container.Register<IStudentSystemApi, StudentSystemApi>(Lifestyle.Singleton);
             container.RegisterDecorator<IStudentSystemApi, StudentSystemApiLoggingDecorator>(Lifestyle.Singleton);
 
-            container.Register(() => new ProfessorsServiceClient(), Lifestyle.Singleton);
+            container.Register(() => new ScoresServiceClient(), Lifestyle.Singleton);
             container.Register(() => new StudentsServiceClient(), Lifestyle.Singleton);
+            container.Register(() => new SemestersServiceClient(), Lifestyle.Singleton);
+            container.Register(() => new ProfessorsServiceClient(), Lifestyle.Singleton);
+            container.Register(() => new DisciplinesServiceClient(), Lifestyle.Singleton);
         }
     }
 }
