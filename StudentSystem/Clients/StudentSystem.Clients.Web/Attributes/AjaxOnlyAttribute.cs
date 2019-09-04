ade4918ecefb9f387e0216fd4f7e94cee17ad0e9
itplamen@gmail.com <itplamen@gmail.com>
2019-09-04 21:56:49 +0300

    Created AjaxOnly attribute

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Attributes/AjaxOnlyAttribute.cs
@@ -0,0 +1,23 @@
+﻿namespace StudentSystem.Clients.Web.Attributes
+{
+    using System;
+
+    using System.Web.Mvc;
+
+    [AttributeUsage(AttributeTargets.Method)]
+    public class AjaxOnlyAttribute : ActionFilterAttribute
+    {
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            if (filterContext.HttpContext.Request.IsAjaxRequest())
+            {
+                base.OnActionExecuting(filterContext);
+            }
+            else
+            {
+                filterContext.HttpContext.Response.StatusCode = 404;
+                filterContext.Result = new HttpNotFoundResult();
+            }
+        }
+    }
+}
