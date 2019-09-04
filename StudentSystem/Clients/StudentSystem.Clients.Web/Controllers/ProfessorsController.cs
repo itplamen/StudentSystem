2da08141566b6f0dd40191a0e1fcde662396dc5e
itplamen@gmail.com <itplamen@gmail.com>
2019-09-05 01:34:03 +0300

    Added MVC endpoint for creating professors

--- StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/ProfessorsController.cs
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/ProfessorsController.cs
@@ -6,6 +6,9 @@
 
     using AutoMapper;
 
+    using Newtonsoft.Json;
+
+    using StudentSystem.Clients.Web.Attributes;
     using StudentSystem.Clients.Web.Models.Professors;
     using StudentSystem.Services.Api.Contracts;
     using StudentSystem.Services.Api.ProfessorsServiceSoap;
@@ -28,6 +31,16 @@
             IEnumerable<ProfessorViewModel> professors = Mapper.Map<IEnumerable<ProfessorViewModel>>(response);
 
             return View(professors);
+        }
+
+        [HttpPost]
+        [AjaxOnly]
+        public async Task<JsonResult> Create(ProfessorCreateViewModel viewModel)
+        {
+            ProfessorRequestModel request = Mapper.Map<ProfessorRequestModel>(viewModel);
+            ProfessorResponseModel response = await studentSystemApi.Execute(professorsClient.CreateAsync, request);
+
+            return Json(JsonConvert.SerializeObject(response));
         }
      }
 }
---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Professors/ProfessorCreateViewModel.cs
@@ -0,0 +1,21 @@
+﻿namespace StudentSystem.Clients.Web.Models.Professors
+{
+    using AutoMapper;
+
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.ProfessorsServiceSoap;
+
+    public class ProfessorCreateViewModel : IMapTo<ProfessorRequestModel>, IHaveCustomMappings
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ProfessorCreateViewModel, ProfessorRequestModel>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(x => x.FirstName))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(x => x.LastName));
+        }
+    }
+}
