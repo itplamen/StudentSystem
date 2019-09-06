1dd8e53b552f4e4f37dfaab92b03b7ca6e357b9c
itplamen@gmail.com <itplamen@gmail.com>
2019-09-06 19:55:27 +0300

    Extracted common logic from professors Index view by creating studentsSystemApi.js

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Scripts/studentSystemApi.js
@@ -0,0 +1,25 @@
+﻿const studentSystemApi = (function () {
+    'use strict';
+
+    function post(url, requestModel, seccess, additionalInfo) {
+        $.ajax({
+            url: url,
+            dataType: 'json',
+            type: 'POST',
+            contentType: 'application/json',
+            data: JSON.stringify(requestModel),
+            success: function (data) {
+                seccess(data, additionalInfo);
+            },
+            error: function (err) {
+                alert('POST request failed!');
+
+                console.log("Error ", err);
+            }
+        });
+    }
+
+    return {
+        post: post
+    };
+}());
