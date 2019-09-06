6f5c43b82423518795ae884ecaa95862f5095b24
itplamen@gmail.com <itplamen@gmail.com>
2019-09-07 00:23:52 +0300

    Created sp_GetProfessorDetails

--- SQL/StudentSystemDb.sql
+++ SQL/StudentSystemDb.sql
@@ -235,9 +235,6 @@ GO
 
 -------------------- Create sp_GetActiveStudentDetails procedure --------------------
 
-USE StudentSystemDb
-GO
-
 CREATE PROCEDURE [dbo].[sp_GetActiveStudentDetails]
 AS
 
@@ -352,3 +349,41 @@ BEGIN
 	DROP TABLE #ActiveStudentDetails
 
 END
+GO
+
+CREATE PROCEDURE [dbo].[sp_GetProfessorDetails]
+	@professorId INT
+AS
+
+BEGIN
+	
+	;WITH ProfessorDetails (
+		ProfessorId, 
+		ProfessorFirstName, 
+		ProfessorLastName, 
+		DisciplineId,
+		DisciplineName,
+		SemesterId,
+		SemesterName,
+		SemesterStarDate,
+		SemesterEndDate)
+	AS
+	(SELECT 
+		professor.Id AS [ProfessorId], 
+		professor.FirstName AS [ProfessorFirstName],
+		professor.LastName AS [ProfessorLastName],
+		discipline.Id AS [DisciplineId],
+		discipline.Name AS [DisciplineName],
+		semester.Id AS [SemesterId] ,
+		semester.Name AS [SemesterName],
+		semester.StartDate AS [SemesterStarDate],
+		semester.EndDate AS [SemesterEndDate]
+	 FROM [dbo].[Professors] professor
+	 JOIN [dbo].[Disciplines] discipline ON professor.Id = discipline.ProfessorId
+	 JOIN [dbo].[Semesters] semester ON semester.Id = discipline.SemesterId
+	 WHERE professor.Id = @professorId)
+
+	 SELECT * FROM ProfessorDetails
+	 
+END
+GO
