dcfa71832447ea6f89d97cc422fcd667cd94bc7e
itplamen@gmail.com <itplamen@gmail.com>
2019-09-08 01:01:13 +0300

    Added logic for creating scores

--- StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/ScoresController.cs
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/ScoresController.cs
@@ -6,6 +6,7 @@
 
     using AutoMapper;
 
+    using StudentSystem.Clients.Web.Attributes;
     using StudentSystem.Clients.Web.Models.Scores;
     using StudentSystem.Services.Api.Contracts;
     using StudentSystem.Services.Api.ScoresServiceSoap;
@@ -28,6 +29,18 @@
             IEnumerable<ScoreResponseViewModel> scores = Mapper.Map<IEnumerable<ScoreResponseViewModel>>(response);
 
             return View(scores);
+        }
+
+        [HttpPost]
+        [AjaxOnly]
+        public async Task<JsonResult> Create(ScoreRequestViewModel viewRequest)
+        {
+            ScoreRequestModel request = Mapper.Map<ScoreRequestModel>(viewRequest);
+            ScoreResponseModel response = await studentSystemApi.Execute(scoresClient.CreateAsync, request);
+
+            ScoreResponseViewModel viewResponse = Mapper.Map<ScoreResponseViewModel>(response);
+
+            return Json(viewResponse);
         }
     }
 }
---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Scores/ScoreRequestViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace StudentSystem.Clients.Web.Models.Scores
+{
+    using AutoMapper;
+
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.ScoresServiceSoap;
+
+    public class ScoreRequestViewModel : IMapTo<ScoreRequestModel>, IHaveCustomMappings
+    {
+        public float Mark { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int DisciplineId { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ScoreRequestViewModel, ScoreRequestModel>()
+                .ForMember(x => x.Mark, opt => opt.MapFrom(x => x.Mark))
+                .ForMember(x => x.StudentId, opt => opt.MapFrom(x => x.StudentId))
+                .ForMember(x => x.DisciplineId, opt => opt.MapFrom(x => x.DisciplineId));
+        }
+    }
+}
