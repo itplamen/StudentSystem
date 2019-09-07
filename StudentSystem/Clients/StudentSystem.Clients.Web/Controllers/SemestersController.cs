809cc08fda1c6d59393348401996d088f71785a1
itplamen@gmail.com <itplamen@gmail.com>
2019-09-07 18:16:08 +0300

    Created semesters controller, Index view and model

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/SemestersController.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentSystem.Clients.Web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    using AutoMapper;
+
+    using StudentSystem.Clients.Web.Models.Semesters;
+    using StudentSystem.Services.Api.Contracts;
+    using StudentSystem.Services.Api.SemestersServiceSoap;
+
+    public class SemestersController : Controller
+    {
+        private readonly IStudentSystemApi studentSystemApi;
+        private readonly SemestersServiceClient semestersClient;
+
+        public SemestersController(IStudentSystemApi studentSystemApi, SemestersServiceClient semestersClient)
+        {
+            this.studentSystemApi = studentSystemApi;
+            this.semestersClient = semestersClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<SemesterResponseModel> response = await studentSystemApi.Execute(semestersClient.AllAsync);
+            IEnumerable<SemesterResponseViewModel> semesters = Mapper.Map<IEnumerable<SemesterResponseViewModel>>(response);
+
+            return View(semesters);
+        }
+    }
+}
---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Semesters/SemesterResponseViewModel.cs
@@ -0,0 +1,32 @@
+﻿namespace StudentSystem.Clients.Web.Models.Semesters
+{
+    using AutoMapper;
+
+    using StudentSystem.Clients.Web.Mappers;
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.SemestersServiceSoap;
+
+    public class SemesterResponseViewModel : IMapFrom<SemesterResponseModel>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public string ModifiedOn { get; set; }
+
+        public string Name { get; set; }
+
+        public string StartDate { get; set; }
+
+        public string EndDate { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<SemesterResponseModel, SemesterResponseViewModel>()
+                .ForMember(x => x.CreatedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.CreatedOn)))
+                .ForMember(x => x.ModifiedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.ModifiedOn)))
+                .ForMember(x => x.StartDate, opt => opt.MapFrom(x => DateTimeMapper.Map(x.StartDate)))
+                .ForMember(x => x.EndDate, opt => opt.MapFrom(x => DateTimeMapper.Map(x.EndDate)));
+        }
+    }
+}
