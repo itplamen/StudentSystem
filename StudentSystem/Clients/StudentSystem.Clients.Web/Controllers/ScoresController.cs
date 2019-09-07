d704aeb5e1dab425ea9cb0f1ac7145deb67390b2
itplamen@gmail.com <itplamen@gmail.com>
2019-09-07 17:41:45 +0300

    Added scores controller, Index view and view model

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Controllers/ScoresController.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentSystem.Clients.Web.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using System.Web.Mvc;
+
+    using AutoMapper;
+
+    using StudentSystem.Clients.Web.Models.Scores;
+    using StudentSystem.Services.Api.Contracts;
+    using StudentSystem.Services.Api.ScoresServiceSoap;
+
+    public class ScoresController : Controller
+    {
+        private readonly IStudentSystemApi studentSystemApi;
+        private readonly ScoresServiceClient scoresClient;
+
+        public ScoresController(IStudentSystemApi studentSystemApi, ScoresServiceClient scoresClient)
+        {
+            this.studentSystemApi = studentSystemApi;
+            this.scoresClient = scoresClient;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> Index()
+        {
+            IEnumerable<ScoreResponseModel> response = await studentSystemApi.Execute(scoresClient.AllAsync);
+            IEnumerable<ScoreResponseViewModel> scores = Mapper.Map<IEnumerable<ScoreResponseViewModel>>(response);
+
+            return View(scores);
+        }
+    }
+}
---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Scores/ScoreResponseViewModel.cs
@@ -0,0 +1,30 @@
+﻿namespace StudentSystem.Clients.Web.Models.Scores
+{
+    using AutoMapper;
+
+    using StudentSystem.Clients.Web.Mappers;
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.ScoresServiceSoap;
+
+    public class ScoreResponseViewModel : IMapFrom<ScoreResponseModel>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public string ModifiedOn { get; set; }
+
+        public float Mark { get; set; }
+
+        public int StudentId { get; set; }
+
+        public int DisciplineId { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<ScoreResponseModel, ScoreResponseViewModel>()
+                .ForMember(x => x.CreatedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.CreatedOn)))
+                .ForMember(x => x.ModifiedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.ModifiedOn)));
+        }
+    }
+}
