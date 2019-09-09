5ea21793148ded65b6da05dc4d3f4bfde783f1f6
itplamen@gmail.com <itplamen@gmail.com>
2019-09-09 21:54:56 +0300

    Created student request and response view models

---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Students/StudentRequestViewModel.cs
@@ -0,0 +1,18 @@
+﻿namespace StudentSystem.Clients.Web.Models.Students
+{
+    using System;
+
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.StudentsServiceSoap;
+
+    public class StudentRequestViewModel : IMapTo<StudentRequestModel>
+    {
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public DateTime DateOfBirth { get; set; }
+    }
+}
---
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Students/StudentResponseViewModel.cs
@@ -0,0 +1,33 @@
+﻿namespace StudentSystem.Clients.Web.Models.Students
+{
+    using AutoMapper;
+
+    using StudentSystem.Clients.Web.Mappers;
+    using StudentSystem.Common.Infrastructure.Mapping;
+    using StudentSystem.Services.Api.StudentsServiceSoap;
+
+    public class StudentResponseViewModel : IMapFrom<StudentResponseModel>, IHaveCustomMappings
+    {
+        public int Id { get; set; }
+
+        public string CreatedOn { get; set; }
+
+        public string ModifiedOn { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string Email { get; set; }
+
+        public string DateOfBirth { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<StudentResponseModel, StudentResponseViewModel>()
+                .ForMember(x => x.CreatedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.CreatedOn)))
+                .ForMember(x => x.ModifiedOn, opt => opt.MapFrom(x => DateTimeMapper.Map(x.ModifiedOn)))
+                .ForMember(x => x.DateOfBirth, opt => opt.MapFrom(x => DateTimeMapper.Map(x.DateOfBirth)));
+        }
+    }
+}
