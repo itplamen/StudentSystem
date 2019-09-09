2789bf3dc2f0d2e4a860ee90d2c72863aa456be2
itplamen@gmail.com <itplamen@gmail.com>
2019-09-09 22:26:50 +0300

    Added missing mapping to the request view model and fixed some front-end bugs

--- StudentSystem/Clients/StudentSystem.Clients.Web/Models/Students/StudentRequestViewModel.cs
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Models/Students/StudentRequestViewModel.cs
@@ -2,10 +2,12 @@
 {
     using System;
 
+    using AutoMapper;
+
     using StudentSystem.Common.Infrastructure.Mapping;
     using StudentSystem.Services.Api.StudentsServiceSoap;
 
-    public class StudentRequestViewModel : IMapTo<StudentRequestModel>
+    public class StudentRequestViewModel : IMapTo<StudentRequestModel>, IHaveCustomMappings
     {
         public string FirstName { get; set; }
 
@@ -14,5 +16,14 @@
         public string Email { get; set; }
 
         public DateTime DateOfBirth { get; set; }
+
+        public void CreateMappings(IProfileExpression configuration)
+        {
+            configuration.CreateMap<StudentRequestViewModel, StudentRequestModel>()
+                .ForMember(x => x.FirstName, opt => opt.MapFrom(x => x.FirstName))
+                .ForMember(x => x.LastName, opt => opt.MapFrom(x => x.LastName))
+                .ForMember(x => x.Email, opt => opt.MapFrom(x => x.Email))
+                .ForMember(x => x.DateOfBirth, opt => opt.MapFrom(x => x.DateOfBirth));
+        }
     }
 }
