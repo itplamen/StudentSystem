4982bb24256173ff0d19c9e842e3853fbb3239a4
itplamen@gmail.com <itplamen@gmail.com>
2019-10-02 00:47:31 +0300

    Added JavaScript promises

--- StudentSystem/Clients/StudentSystem.Clients.Web/Scripts/studentSystemApi.js
+++ StudentSystem/Clients/StudentSystem.Clients.Web/Scripts/studentSystemApi.js
@@ -1,40 +1,32 @@
 ﻿const studentSystemApi = (function () {
     'use strict';
 
-    function post(url, requestModel, seccess, additionalInfo) {
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            type: 'POST',
-            contentType: 'application/json',
-            data: JSON.stringify(requestModel),
-            success: function (data) {
-                seccess(data, additionalInfo);
-            },
-            error: function (err) {
-                alert('POST request failed!');
-
-                console.log("Error ", err);
-            }
+    function send(method, url, requestModel) {
+        let promise = new Promise(function (resolve, reject) {
+            $.ajax({
+                url: url,
+                dataType: 'json',
+                method: method,
+                contentType: 'application/json',
+                data: JSON.stringify(requestModel),
+                success: function (data) {
+                    resolve(data);
+                },
+                error: function (err) {
+                    reject(err);
+                }
+            });
         });
+
+        return promise;
     }
 
-    function get(url, requestModel, seccess, additionalInfo) {
-        $.ajax({
-            url: url,
-            dataType: 'json',
-            type: 'GET',
-            contentType: 'application/json',
-            data: requestModel,
-            success: function (data) {
-                seccess(data, additionalInfo);
-            },
-            error: function (err) {
-                alert('GET request failed!');
+    function post(url, requestModel) {
+        return send('POST', url, requestModel);
+    }
 
-                console.log("Error ", err);
-            }
-        });
+    function get(url, requestModel) {
+        return send('GET', url, requestModel);
     }
 
     return {
